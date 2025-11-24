1471f05a22753dc784c71eac9b25d73ab239634c
arsen <arsen.yeghiazarian@gmail.com>
2025-11-24 18:08:09 +0400

    Add product and category card components
    Introduces BuyBtn, ProductCard, and skeleton loader components for product and category cards. These components support product display, cart interactions, and loading states for improved user experience.

---
+++ src/components/ProductCardSkeleton.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-card>
+    <v-skeleton-loader type="image" height="200"></v-skeleton-loader>
+    <v-card-actions>
+      <v-skeleton-loader type="text" width="100"></v-skeleton-loader>
+      <v-spacer></v-spacer>
+      <v-skeleton-loader type="button" width="100"></v-skeleton-loader>
+    </v-card-actions>
+  </v-card>
+</template>
---
+++ src/components/ProductDetailsSkeleton.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container max-width="1120">
+    <v-row>
+      <v-col cols="12" sm="6">
+        <v-skeleton-loader type="image" height="500"></v-skeleton-loader>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-skeleton-loader type="heading" class="mb-4"></v-skeleton-loader>
+        <v-skeleton-loader type="paragraph" class="mb-2"></v-skeleton-loader>
+        <v-skeleton-loader type="paragraph" class="mb-4"></v-skeleton-loader>
+        <v-skeleton-loader type="text" width="150" class="mb-4"></v-skeleton-loader>
+        <v-skeleton-loader type="button" width="120"></v-skeleton-loader>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
