8e638e4dbdd70f7ef0532c780331b85cdfc3756b
arsen <arsen.yeghiazarian@gmail.com>
2025-11-24 19:44:50 +0400

    Update CartIcon to use programmatic navigation

--- src/components/CartIcon.vue
+++ src/components/CartIcon.vue
@@ -1,11 +1,17 @@
 <script lang="ts" setup>
   import { useCartStore } from '@/store/cart';
+  import { useRouter } from 'vue-router';
 
+  const router = useRouter();
   const cart = useCartStore();
+
+  const navigateToCart = () => {
+    router.push('/cart');
+  };
 </script>
 
 <template>
-  <v-btn icon flat variant="icon" class="position-relative" to="/cart">
+  <v-btn icon @click="navigateToCart()">
     <v-badge :content="cart.totalItems" size="small" location="top right">
       <v-icon icon="mdi-cart-outline"></v-icon>
     </v-badge>
